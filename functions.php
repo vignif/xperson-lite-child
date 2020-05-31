2a79fac87e7b91e5145d3d5b386b5f281d481a12
francesco <vignif@gmail.com>
2020-05-31 19:22:31 +0200

    added fontawesome

--- functions.php
+++ functions.php
@@ -4,3 +4,19 @@ function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
 ?>
+
+
+<?php
+add_action( 'wp_enqueue_scripts', 'prefix_enqueue_awesome' );
+/**
+ * Register and load font awesome CSS files using a CDN.
+ */
+function prefix_enqueue_awesome() {
+	wp_enqueue_style( 
+		'font-awesome-5', 
+		'https://use.fontawesome.com/releases/v5.3.0/css/all.css', 
+		array(), 
+		'5.3.0' 
+	);
+}
+?>
