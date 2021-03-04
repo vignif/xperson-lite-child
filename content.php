fd0eaa76e4e23962f9e5d5fccc0b8d6fec793d8c
francesco <vignif@gmail.com>
2021-03-04 10:24:33 +0100

    Add post date

--- content.php
+++ content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template part for displaying content
  *
@@ -11,71 +12,77 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<?php xperson_lite_post_thumbnail(); ?>
-	
+
 	<div class="blog-post-content">
 		<header class="entry-header">
-			<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
-				<span class="sticky-post"><i class="fa fa-star-o"></i> <?php esc_html_e( 'Featured', 'xperson-lite' ); ?></span>
+			<?php if (is_sticky() && is_home() && !is_paged()) : ?>
+				<span class="sticky-post"><i class="fa fa-star-o"></i> <?php esc_html_e('Featured', 'xperson-lite'); ?></span>
 			<?php endif; ?>
 			<!-- <div class="post-meta">
 				<span class="the-category"> 
-					<?php //the_category( ', ' ); ?>
+					<?php //the_category( ', ' ); 
+					?>
 				</span>
 			</div> -->
 
-			<?php the_title( sprintf( '<a href="%s" rel="bookmark"><h2 class="entry-title">', esc_url( get_permalink() ) ), '</h2>' ); ?>
+			<?php the_title(sprintf('<a href="%s" rel="bookmark"><h2 class="entry-title">', esc_url(get_permalink())), '</h2>'); ?>
 		</header><!-- .entry-header -->
-
+		<div class="entry-summary clearfix">
+			<?php the_date('d/m/Y', '<p>', '</p>'); ?>
+		</div>
 		<div class="entry-summary clearfix">
 			<?php
-				/* translators: %s: Name of current post */
-				the_excerpt( sprintf(
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'xperson-lite' ),
-					get_the_title()
-				) );
+			/* translators: %s: Name of current post */
+			the_excerpt(sprintf(
+				__('Continue reading<span class="screen-reader-text"> "%s"</span>', 'xperson-lite'),
+				get_the_title()
+			));
 
-				wp_link_pages( array(
-					'before'	  => '<div class="page-links"><span class="page-links-title">' . esc_html__( 'Pages:', 'xperson-lite' ) . '</span>',
-					'after'	   => '</a></div>',
-					'link_before' => ' <span>',
-					'link_after'  => '</span> ',
-					'pagelink'	=> '<span class="screen-reader-text">' . esc_html__( 'Page', 'xperson-lite' ) . ' </span>%',
-					'separator'   => '<span class="screen-reader-text">, </span>',
-				) );
+			wp_link_pages(array(
+				'before'	  => '<div class="page-links"><span class="page-links-title">' . esc_html__('Pages:', 'xperson-lite') . '</span>',
+				'after'	   => '</a></div>',
+				'link_before' => ' <span>',
+				'link_after'  => '</span> ',
+				'pagelink'	=> '<span class="screen-reader-text">' . esc_html__('Page', 'xperson-lite') . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			));
 			?>
 		</div><!-- .entry-content -->
 		<!-- <hr> -->
 		<!-- <footer class="entry-footer"> -->
-			<!-- <div class="entry-meta">
+		<!-- <div class="entry-meta">
 				<ul class="list-inline">
 					<li>
 						<span class="the-author">
-							<?php //the_author_posts_link(); ?>
+							<?php //the_author_posts_link(); 
+							?>
 						</span>
 					</li>
 					<li>
 						<span class="the-time">
-							<?php //echo wp_kses_post( xperson_lite_post_published_link() ); ?>
+							<?php //echo wp_kses_post( xperson_lite_post_published_link() ); 
+							?>
 						</span>
 					</li>
 					<li>
 						<span class="the-comments">
-							<?php //comments_popup_link( sprintf( esc_html__( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'xperson-lite' ), get_the_title() ) ); ?>
+							<?php //comments_popup_link( sprintf( esc_html__( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'xperson-lite' ), get_the_title() ) ); 
+							?>
 						</span>
 					</li>
 				</ul>
 			</div>.entry-meta -->
-			<?php
-				edit_post_link(
-					sprintf(
-						/* translators: %s: Name of current post */
-						__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'xperson-lite' ),
-						get_the_title()
-					),
-					'<div class="edit-link">',
-					'</div>'
-				);
-			?>
+		<?php
+		edit_post_link(
+			sprintf(
+				/* translators: %s: Name of current post */
+				__('Edit<span class="screen-reader-text"> "%s"</span>', 'xperson-lite'),
+				get_the_title()
+			),
+			'<div class="edit-link">',
+			'</div>'
+		);
+		?>
 		<!-- </footer>.entry-footer -->
 	</div>
 </article><!-- #post-## -->
